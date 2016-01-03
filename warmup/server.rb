1d5d4b7fffbc228bba4b396e1b1ec3b8adafd29d
datyou <3lsoar@gamil.com>
2016-01-03 17:40:07 +0900

    13.3

--- warmup/server.rb
+++ warmup/server.rb
@@ -3,26 +3,42 @@ require 'socket'
 port = 2000
 server = TCPServer.open(port)
 
+socks = [server]
 
-
-requetst = ""
-while buf = conn.gets
-    requetst << buf
-    next unless requetst =~ /\r\n\r\n/
-    body =<<EOF
+while true
+    ns = select(socks)
+    next if ns == nil
+    for s in ns[0]
+        if s == server
+            conn = s.accept
+            socks << conn
+            puts "acceptd #{conn}"
+            puts "wainting #{socks}"
+        else
+            if s.eof?
+                s.close
+                socks.delete(s)
+                puts "connection done #{s}"
+            else
+                requetst = ""
+                while buf = s.gets
+                requetst << buf
+                next unless requetst =~ /\r\n\r\n/
+                body =<<EOF
 <html><body>Hello World</body></html>
 EOF
 
-    response = <<EOF
+                response = <<EOF
 HTTP/1.0 200 OK\r
 Conntent-Type: text/html\r
 Content-Length: #{body.size}\r
 \r
 #{body}
 EOF
-    conn.write(response)
-    break
+                s.write(response)
+                break
+            end
+        end
+    end
+    end
 end
-
-conn.close
-server.close
