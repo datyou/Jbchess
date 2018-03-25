1eec9d6e4d4549fd8adbbe7e41655d6d305a32af
datyou <3lsoar@gamil.com>
2018-03-26 02:06:09 +0900

    挙動デバック

--- JpChess.rb
+++ JpChess.rb
@@ -8,67 +8,80 @@ class Field
     end
     attr_accessor :field
 
-    #盤の表示
+#盤の表示
     def print_field(array)
         for i in 0..8
             for j in 0..8
-            print array[i][8-j]
+            print array[8-j][i]
             end
             puts
         end
     end
 
+#駒の移動
     def move(line, column, newline, newcolumn, array)
         array[newline.to_i-1][newcolumn.to_i-1] = array[line.to_i-1][column.to_i-1]
         array[line.to_i-1][column.to_i-1] = "□"
     end
 
+#駒の移動ができるか
+    def move_check(line, column, newline, newcolumn, array)
+        if array[line][column] == "□"
+
+
+        end
+    end
+
+#移動先に駒あるか
+    def piece_check(array)
+
+    end
 end
 
-
+#fiieldオブジェクト作成
 allField = Field.new
+
 #駒オブジェクトの生成と表示
-
-allField.field[2][0] = "歩"
-allField.field[2][1] = "歩"
+allField.field[0][2] = "歩"
+allField.field[1][2] = "歩"
 allField.field[2][2] = "歩"
-allField.field[2][3] = "歩"
-allField.field[2][4] = "歩"
-allField.field[2][5] = "歩"
-allField.field[2][6] = "歩"
-allField.field[2][7] = "歩"
-allField.field[2][8] = "歩"
-allField.field[1][1] = "角"
-allField.field[1][7] = "飛"
-allField.field[0][0] = "香"
-allField.field[0][1] = "桂"
-allField.field[0][2] = "銀"
-allField.field[0][3] = "金"
-allField.field[0][4] = "王"
-allField.field[0][5] = "金"
-allField.field[0][6] = "銀"
-allField.field[0][7] = "桂"
-allField.field[0][8] = "香"
-
-allField.field[6][0] = "歩"
-allField.field[6][1] = "歩"
+allField.field[3][2] = "歩"
+allField.field[4][2] = "歩"
+allField.field[5][2] = "歩"
 allField.field[6][2] = "歩"
-allField.field[6][3] = "歩"
-allField.field[6][4] = "歩"
-allField.field[6][5] = "歩"
-allField.field[6][6] = "歩"
-allField.field[6][7] = "歩"
-allField.field[6][8] = "歩"
+allField.field[7][2] = "歩"
+allField.field[8][2] = "歩"
+allField.field[1][1] = "角"
 allField.field[7][1] = "飛"
-allField.field[7][7] = "角"
+allField.field[0][0] = "香"
+allField.field[1][0] = "桂"
+allField.field[2][0] = "銀"
+allField.field[3][0] = "金"
+allField.field[4][0] = "王"
+allField.field[5][0] = "金"
+allField.field[6][0] = "銀"
+allField.field[7][0] = "桂"
 allField.field[8][0] = "香"
-allField.field[8][1] = "桂"
-allField.field[8][2] = "銀"
-allField.field[8][3] = "金"
-allField.field[8][4] = "玉"
-allField.field[8][5] = "金"
-allField.field[8][6] = "銀"
-allField.field[8][7] = "桂"
+
+allField.field[0][6] = "歩"
+allField.field[1][6] = "歩"
+allField.field[2][6] = "歩"
+allField.field[3][6] = "歩"
+allField.field[4][6] = "歩"
+allField.field[5][6] = "歩"
+allField.field[6][6] = "歩"
+allField.field[7][6] = "歩"
+allField.field[8][6] = "歩"
+allField.field[7][7] = "角"
+allField.field[1][7] = "飛"
+allField.field[0][8] = "香"
+allField.field[1][8] = "桂"
+allField.field[2][8] = "銀"
+allField.field[3][8] = "金"
+allField.field[4][8] = "王"
+allField.field[5][8] = "金"
+allField.field[6][8] = "銀"
+allField.field[7][8] = "桂"
 allField.field[8][8] = "香"
 
 
